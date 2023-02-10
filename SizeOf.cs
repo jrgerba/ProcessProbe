ace3a7912c2f3e75562d92e4131c244b4c81e6c8
jrgerba <joseph.gerba130@gmail.com>
2023-02-09 21:39:12 -0500

    Dynamic sizeof generator

---
+++ SizeOf.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Emit;
+
+namespace ProcessProbe;
+
+public static class SizeOf<T>
+{
+    public static readonly int Size;
+
+    static SizeOf()
+    {
+        DynamicMethod dm = new("GenericSizeOf", typeof(uint), Array.Empty<Type>());
+        ILGenerator il = dm.GetILGenerator();
+        il.Emit(OpCodes.Sizeof, typeof(T));
+        il.Emit(OpCodes.Ret);
+        Size = (int)dm.Invoke(null, null);
+    }
+}
