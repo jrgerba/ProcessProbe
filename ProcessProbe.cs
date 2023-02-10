0efec8fede69bf57ce252cb721ab7d3801165052
jrgerba <joseph.gerba130@gmail.com>
2023-02-09 21:39:30 -0500

    Create ProcessProbe class

---
+++ ProcessProbe.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using ProcessProbe.MemoryInterface;
+
+namespace ProcessProbe;
+
+public class ProcessProbe
+{
+    private readonly IMemoryInterface _memory;
+    private readonly Process _proc;
+
+    public unsafe int Read<T>(nint address, out T value) where T : struct
+    {
+        EnforceTypeSafety<T>();
+
+        value = default;
+
+        Span<byte> buffer = new(&value, SizeOf<T>.Size);
+
+        return _memory.Read(address, buffer);
+    }
+
+    public unsafe int Write<T>(nint address, out T value) where T : struct
+    {
+        EnforceTypeSafety<T>();
+
+        Span<byte> buffer = new(&value, SizeOf<T>.Size);
+
+        return _memory.Write(address, buffer);
+    }
+
+    public unsafe int ReadArray<T>(nint address, Span<T> array) where T : struct
+    {
+        EnforceTypeSafety<T>();
+
+        fixed (void* bufferPtr = &array.GetPinnableReference())
+        {
+            Span<byte> buffer = new(bufferPtr, SizeOf<T>.Size * array.Length);
+
+            return _memory.Read(address, buffer);
+        }
+    }
+
+    public unsafe int WriteArray<T>(nint address, Span<T> array) where T : struct
+    {
+        EnforceTypeSafety<T>();
+
+        fixed (void* bufferPtr = &array.GetPinnableReference())
+        {
+            Span<byte> buffer = new(bufferPtr, SizeOf<T>.Size * array.Length);
+
+            return _memory.Write(address, buffer);
+        }
+    }
+
+    private void EnforceTypeSafety<T>() { }
+}
