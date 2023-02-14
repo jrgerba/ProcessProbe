3890ca76407e0b2495221283e1929017a97a5ca6
jrgerba <joseph.gerba130@gmail.com>
2023-02-13 23:19:46 -0500

    Converted Read/Write<T> to an unmanaged type, removing the need to enforce type safety

--- Probe.cs
+++ Probe.cs
@@ -10,109 +10,71 @@ namespace ProcessProbe;
 public class Probe
 {
     // Static //
-    private static Dictionary<Type, bool> _safetyLookup = new(); 
-
-    private static void EnforceTypeSafety<T>() => EnforceTypeSafety(typeof(T));
-
-    private static void EnforceTypeSafety(Type t)
-    {
-        if (_safetyLookup.TryGetValue(t, out bool isSafe))
-        {
-            if (isSafe)
-                return;
-
-            throw new UnsafeTypeException("The given type cannot be a reference");
-        }
-
-        if (t.IsPrimitive)
-            return;
-
-        if (t.IsClass)
-            throw new UnsafeTypeException("The given type cannot be a reference");
-
-        FieldInfo[] fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-
-        for (int i = 0; i < fields.Length; i++)
-        {
-            FieldInfo f = fields[i];
-            EnforceTypeSafety(f.FieldType);
-        }
-
-        _safetyLookup.Add(t, true);
-    }
-
+    private static readonly Dictionary<Type, bool> SafetyLookup = new(); 
+    
     // Instance //
-
     private readonly IMemoryInterface _memory;
     private readonly Process _proc;
 
-    public unsafe int Read<T>(nint address, out T value) where T : struct
+    public unsafe int Read<T>(nint address, out T value) where T : unmanaged
     {
-        EnforceTypeSafety<T>();
-
         value = default;
 
         fixed (void* valueAddr = &value)
         {
-            Span<byte> buffer = new(valueAddr, SizeOf<T>.Size);
+            Span<byte> buffer = new(valueAddr, sizeof(T));
 
             return _memory.Read(address, buffer);
         }
     }
 
-    public int Read<T>(nint address, int offset, out T value) where T : struct => Read(address + offset, out value);
+    public int Read<T>(nint address, int offset, out T value) where T : unmanaged => Read(address + offset, out value);
 
-    public int Read<T>(string exportName, int offset, out T value) where T : struct =>
+    public int Read<T>(string exportName, int offset, out T value) where T : unmanaged =>
         Read(_memory.GetExportedObject(exportName), offset, out value);
 
-    public unsafe int Write<T>(nint address, T value) where T : struct
+    public unsafe int Write<T>(nint address, T value) where T : unmanaged
     {
-        EnforceTypeSafety<T>();
-
-        Span<byte> buffer = new(&value, SizeOf<T>.Size);
+        Span<byte> buffer = new(&value, sizeof(T));
 
         return _memory.Write(address, buffer);
     }
 
-    public int Write<T>(nint address, int offset, T value) where T : struct => Write(address + offset, value);
+    public int Write<T>(nint address, int offset, T value) where T : unmanaged => Write(address + offset, value);
 
-    public int Write<T>(string exportName, int offset, T value) where T : struct =>
+    public int Write<T>(string exportName, int offset, T value) where T : unmanaged =>
         Write(_memory.GetExportedObject(exportName), offset, value);
 
-    public unsafe int ReadArray<T>(nint address, Span<T> array) where T : struct
+    public unsafe int ReadArray<T>(nint address, Span<T> array) where T : unmanaged
     {
-        EnforceTypeSafety<T>();
-
         fixed (void* bufferPtr = array)
         {
-            Span<byte> buffer = new(bufferPtr, SizeOf<T>.Size * array.Length);
+            Span<byte> buffer = new(bufferPtr, sizeof(T) * array.Length);
 
             return _memory.Read(address, buffer);
         }
     }
 
-    public int ReadArray<T>(nint address, int offset, Span<T> array) where T : struct =>
+    public int ReadArray<T>(nint address, int offset, Span<T> array) where T : unmanaged =>
         ReadArray(address + offset, array);
 
-    public int ReadArray<T>(string exportName, int offset, Span<T> array) where T : struct =>
+    public int ReadArray<T>(string exportName, int offset, Span<T> array) where T : unmanaged =>
         ReadArray(_memory.GetExportedObject(exportName) + offset, array);
 
-    public unsafe int WriteArray<T>(nint address, Span<T> array) where T : struct
+    public unsafe int WriteArray<T>(nint address, Span<T> array) where T : unmanaged
     {
-        EnforceTypeSafety<T>();
-
         fixed (void* bufferPtr = array)
         {
-            Span<byte> buffer = new(bufferPtr, SizeOf<T>.Size * array.Length);
+            Span<byte> buffer = new(bufferPtr, sizeof(T) * array.Length);
 
             return _memory.Write(address, buffer);
         }
     }
 
-    public int WriteArray<T>(nint address, int offset, Span<T> array) where T : struct =>
+    public int WriteArray<T>(nint address, int offset, Span<T> array) where T : unmanaged =>
         WriteArray(address + offset, array);
 
-    public int WriteArray<T>(string exportName, int offset, Span<T> array) where T : struct =>
+    public int WriteArray<T>(string exportName, int offset, Span<T> array) where T : unmanaged =>
         WriteArray(_memory.GetExportedObject(exportName), offset, array);
 
     public Probe(Process proc)
