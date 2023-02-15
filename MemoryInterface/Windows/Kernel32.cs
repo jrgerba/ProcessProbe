9c0fd7f8a6ef00f7dbfaa1861c1c1894e48315cb
jrgerba <joseph.gerba130@gmail.com>
2023-02-14 20:06:39 -0500

    Moved Kernel32 to its own static class

---
+++ MemoryInterface/Windows/Kernel32.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ProcessProbe.MemoryInterface.Windows
+{
+    internal static class Kernel32
+    {
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern nint OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool CloseHandle(nint hObject);
+
+        [DllImport("Kernel32.dll")]
+        public static extern unsafe bool WriteProcessMemory(nint hProcess, nint lpBaseAddress, byte* lpBuffer, int nSize,
+            out int lpNumberOfBytesWritten);
+        [DllImport("Kernel32.dll")]
+        public static extern unsafe bool ReadProcessMemory(nint hProcess, nint lpBaseAddress, byte* lpBuffer, int nSize,
+            out int lpNumberOfBytesRead);
+
+    }
+}
