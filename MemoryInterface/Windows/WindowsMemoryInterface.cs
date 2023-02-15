065b761db4795367af107e0efd78b96696b99bc2
jrgerba <joseph.gerba130@gmail.com>
2023-02-14 20:08:03 -0500

    Added namespace using to PeNet.Header.Pe

--- MemoryInterface/Windows/WindowsMemoryInterface.cs
+++ MemoryInterface/Windows/WindowsMemoryInterface.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using PeNet;
+using PeNet.Header.Pe;
 
 namespace ProcessProbe.MemoryInterface.Windows
 {
